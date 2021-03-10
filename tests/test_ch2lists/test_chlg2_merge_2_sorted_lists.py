41f4ae2ccca19eb6e16d38e3eb02251b36e90af2
David Dansby <ddansby89@gmail.com>
2021-03-10 03:13:45 -0800

    Remove unused import

--- tests/test_ch2lists/test_chlg2_merge_2_sorted_lists.py
+++ tests/test_ch2lists/test_chlg2_merge_2_sorted_lists.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from ds_coding_interviews_in_python.ch2lists.chlg2_merge_2_sorted_lists import merge_lists_inplace, \
 	merge_lists_new
 
@@ -9,6 +7,7 @@ def test_merge_lists_new() -> None:
 	lst2 = [2, 4, 5, 7, 8]
 	exp_result = [1, 2, 3, 4, 5, 5, 7, 8, 8]
 	assert merge_lists_new(lst1, lst2) == exp_result
+
 
 def test_merge_lists_inplace() -> None:
 	lst1 = [1, 3, 5, 8]
