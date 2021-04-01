61e2a9c3080730443833ff8666731f70d6c92e95
David Dansby <ddansby89@gmail.com>
2021-04-01 01:45:38 -0700

    Solve chlg11 max sum sublist with Kadane's Algo

--- ds_coding_interviews_in_python/ch2lists/chlg11_max_sum_sublist.py
+++ ds_coding_interviews_in_python/ch2lists/chlg11_max_sum_sublist.py
@@ -1,4 +1,21 @@
 from typing import List
 
-def find_max_sum_sublist(lst:List[int]) -> int:
-    pass
+
+def find_max_sum_sublist(lst: List[int]) -> int:
+    if len(lst) < 1:
+        return 0
+
+    curr_max = lst[0]
+    global_max = lst[0]
+
+    lst_len = len(lst)
+
+    for i in range(1, lst_len):
+        if curr_max < 0:
+            curr_max = lst[i]
+        else:
+            curr_max += lst[i]
+        if global_max < curr_max:
+            global_max = curr_max
+
+    return global_max
--- tests/test_ch2lists/test_chlg11_max_sum_sublist.py
+++ tests/test_ch2lists/test_chlg11_max_sum_sublist.py
@@ -1,6 +1,15 @@
 from ds_coding_interviews_in_python.ch2lists.chlg11_max_sum_sublist import find_max_sum_sublist
 
+
 def test_find_max_sum_sublist() -> None:
     lst = [-4, 2, -5, 1, 2, 3, 6, -5, 1]
     result = 12
     assert find_max_sum_sublist(lst) == result
+
+    lst2 = list()
+    result2 = 0
+    assert find_max_sum_sublist(lst2) == result2
+
+    lst3 = [-1, -2, -1, -1, -2]
+    result3 = -1
+    assert find_max_sum_sublist(lst3) == result3
