b8ebcbcd5946f2e54957596eeadd7bca98c79018
David Dansby <ddansby89@gmail.com>
2021-04-01 00:14:31 -0700

    Solve chlg 10 with constant space solution

--- ds_coding_interviews_in_python/ch2lists/chlg10_rearrange_sorted_list.py
+++ ds_coding_interviews_in_python/ch2lists/chlg10_rearrange_sorted_list.py
@@ -19,3 +19,25 @@ def max_min(lst: List[int]) -> List[int]:
         # if middle val then append
         result.append(lst[len(lst) // 2])
     return result
+
+# O(n) but space complexity with this is constant at O(1)
+def max_mix_space_1(lst:List[int]) -> List[int]:
+    if len(lst) is 0:
+        return []
+    max_idx = len(lst)-1 # last index
+    max_ele = lst[-1] + 1
+    min_idx = 0 # first index
+
+    for i in range(len(lst)):
+        # even number means max element to append
+        if i % 2 == 0:
+            lst[i] += (lst[max_idx] % max_ele) * max_ele
+            max_idx -= 1
+        # odd number means min number
+        else:
+            lst[i] += (lst[min_idx] % max_ele) * max_ele
+            min_idx += 1
+
+    for i in range(len(lst)):
+        lst[i] = lst[i] // max_ele
+    return lst
--- tests/test_ch2lists/test_chlg10_rearrange_sorted_list.py
+++ tests/test_ch2lists/test_chlg10_rearrange_sorted_list.py
@@ -1,4 +1,8 @@
-from ds_coding_interviews_in_python.ch2lists.chlg10_rearrange_sorted_list import max_min
+from ds_coding_interviews_in_python.ch2lists.chlg10_rearrange_sorted_list import (
+    max_min,
+    max_mix_space_1,
+)
+
 
 def test_max_min() -> None:
     lst = [1, 2, 3, 4, 5]
@@ -8,3 +12,25 @@ def test_max_min() -> None:
     lst2 = [1]
     expect2 = [1]
     assert max_min(lst2) == expect2
+
+    lst3 = [1, 1, 1, 1]
+    expect3 = [1, 1, 1, 1]
+    assert max_min(lst3) == expect3
+
+
+def test_max_mix_space_1() -> None:
+    lst = [1, 2, 3, 4, 5]
+    expect = [5, 1, 4, 2, 3]
+    assert max_mix_space_1(lst) == expect
+
+    lst2 = [1]
+    expect2 = [1]
+    assert max_mix_space_1(lst2) == expect2
+
+    lst3 = [1, 1, 1, 1]
+    expect3 = [1, 1, 1, 1]
+    assert max_min(lst3) == expect3
+
+    lst4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    expect4 = [11, 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
+    assert max_mix_space_1(lst4) == expect4
