fbf77d92979cc3d98f6be28b5ec3582ed4c2444d
David Dansby <ddansby89@gmail.com>
2021-04-21 04:18:39 -0700

    Optimize find next greater ele func with MyStack

--- ds_coding_interviews_in_python/ch4stackqueue/chlg7_next_greater_ele.py
+++ ds_coding_interviews_in_python/ch4stackqueue/chlg7_next_greater_ele.py
@@ -1,7 +1,18 @@
 from typing import List
 
+from ds_coding_interviews_in_python.ch4stackqueue.stack import MyStack
+
 
 def next_greater_ele_brute_force(lst: List[int]) -> List[int]:
+    """
+    For each element in list, function returns the first element to
+    the right that is larger than the element being compared to.
+    Time complexity is O(n^2) because we iterate through the list once
+    for each element at worst.
+
+    :param lst: List[int]
+    :return: List[int]
+    """
     if len(lst) == 0:
         return lst
     for i in range(len(lst) - 1):
@@ -14,3 +25,37 @@ def next_greater_ele_brute_force(lst: List[int]) -> List[int]:
                 break
     lst[-1] = -1
     return lst
+
+
+def next_greater_ele_stack(lst: List[int]) -> List[int]:
+    """
+    For each element in list, function returns the first element to
+    the right that is larger than the element being compared to.
+    Since we only iterate through the list once the Time Complexity is
+    only O(n) instead of O(n^2) like the brute force method above.
+
+    :param lst: List[int]
+    :return: List[int]
+    """
+    s = MyStack()
+    res = [-1] * len(lst)
+    # Reverse iterate through the list
+    for i in range(len(lst) - 1, -1, -1):
+
+        # While stack has elements and current element is greater
+        # than top element of stack pop all elements
+        while s.is_empty() is False and lst[i] >= s.top():
+
+            s.pop()
+
+        # If stack has an element the top element
+        # that is greater than the ith element
+        if s.is_empty() is False:
+
+            res[i] = s.top()
+
+        # Push in the current element into the stack
+
+        s.push(lst[i])
+
+    return res
--- tests/test_ch4stackqueue/test_chlg7_next_greater_ele.py
+++ tests/test_ch4stackqueue/test_chlg7_next_greater_ele.py
@@ -2,6 +2,7 @@ from typing import List
 
 from ds_coding_interviews_in_python.ch4stackqueue.chlg7_next_greater_ele import (
     next_greater_ele_brute_force,
+    next_greater_ele_stack,
 )
 
 
@@ -15,3 +16,15 @@ def test_next_greater_ele_empty() -> None:
     lst: List[int] = list()
     result: List[int] = list()
     assert next_greater_ele_brute_force(lst) == result
+
+
+def test_next_greater_ele_stack() -> None:
+    lst = [4, 6, 3, 2, 8, 1, 9, 9, 9]
+    result = [6, 8, 8, 8, 9, 9, -1, -1, -1]
+    assert next_greater_ele_stack(lst) == result
+
+
+def test_next_greater_ele_stack_empty() -> None:
+    lst: List[int] = list()
+    result: List[int] = list()
+    assert next_greater_ele_stack(lst) == result
