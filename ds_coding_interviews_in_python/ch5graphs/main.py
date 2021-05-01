6eb685b3cad8557bc7384d09fc02232256aefc8d
David Dansby <ddansby89@gmail.com>
2021-05-01 16:36:10 -0700

    Implement bfs algo then returns all vertices after traversing from start

--- ds_coding_interviews_in_python/ch5graphs/main.py
+++ ds_coding_interviews_in_python/ch5graphs/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List, Tuple
 
 from ds_coding_interviews_in_python.ch4stackqueue.myqueue import MyQueue
 from ds_coding_interviews_in_python.ch5graphs.graph import Graph
@@ -45,5 +45,48 @@ def bfs_simple_dict(graph, start_node):
             if neighbor not in visited:
                 visited.append(neighbor)
                 queue.append(neighbor)
+
+    return result
+
+
+def bfs_helper(g: Graph, source: Any, visited: List[Any]) -> Tuple[str, List[Any]]:
+    result = ""
+    # Create Queue(implemented in previous lesson) for Breadth First Traversal
+    # and enqueue source in it
+    queue = MyQueue()
+    queue.enqueue(source)
+    visited[source] = True  # Mark as visited
+    # Traverse while queue is not empty
+    while not queue.is_empty():
+        # Dequeue a vertex/node from queue and add it to result
+        curr_node = queue.dequeue()
+        result += str(curr_node)
+        # Get adjacent vertices to the current_node from the list,
+        # and if they are not already visited then enqueue them in the Queue
+        temp = g.array[curr_node].get_head()
+        while temp is not None:
+            if not visited[temp.data]:
+                queue.enqueue(temp.data)
+                visited[temp.data] = True  # Visit the current Node
+            temp = temp.next_element
+    return result, visited
+
+
+def bfs(g: Graph, source: Any) -> str:
+    result: str = ""
+    num_v: int = g.vertices
+    if num_v == 0:
+        return result
+
+    visited = list()
+    for i in range(num_v):
+        visited.append(False)
+    # start from the source
+    result, visited = bfs_helper(g, source, visited)
+    # visit the remaining nodes
+    for i in range(num_v):
+        if not visited[i]:
+            result_new, visited = bfs_helper(g, i, visited)
+            result += result_new
 
     return result
--- tests/test_ch5graphs/test_graph.py
+++ tests/test_ch5graphs/test_graph.py
@@ -1,5 +1,5 @@
 from ds_coding_interviews_in_python.ch5graphs.graph import Graph
-from ds_coding_interviews_in_python.ch5graphs.main import bfs_simple, bfs_simple_dict
+from ds_coding_interviews_in_python.ch5graphs.main import bfs, bfs_simple, bfs_simple_dict
 
 
 def test_bfs_simple() -> None:
@@ -9,6 +9,25 @@ def test_bfs_simple() -> None:
     g.add_edge(1, 3)
     g.add_edge(2, 3)
     assert bfs_simple(g, 0) == "0123"
+
+
+def test_bfs_simple_empty() -> None:
+    g = Graph(0)
+    assert bfs_simple(g, 0) == ""
+
+
+def test_bfs() -> None:
+    g = Graph(4)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(1, 3)
+    g.add_edge(2, 3)
+    assert bfs(g, 0) == "0123"
+
+
+def test_bfs_empty() -> None:
+    g = Graph(0)
+    assert bfs(g, 0) == ""
 
 
 def test_bfs_simple_dict() -> None:
