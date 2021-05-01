5536065173469e952b8b06a5b1f15c31ff5e6943
David Dansby <ddansby89@gmail.com>
2021-05-01 15:54:14 -0700

    Create simple bfs algo that return results depending on starting node

--- ds_coding_interviews_in_python/ch5graphs/main.py
+++ ds_coding_interviews_in_python/ch5graphs/main.py
@@ -1,18 +1,10 @@
+from typing import Any
+
 from ds_coding_interviews_in_python.ch4stackqueue.myqueue import MyQueue
 from ds_coding_interviews_in_python.ch5graphs.graph import Graph
 
-g = Graph(4)
-g.add_edge(0, 1)
-g.add_edge(0, 2)
-print(g.array[0].to_list())
-g.add_edge(1, 3)
-g.add_edge(2, 3)
-g.print_graph()
 
-print(g.array[1].get_head().data)
-
-
-def bfs(g: Graph, source: int) -> str:
+def bfs_simple(g: Graph, source: Any) -> str:
     result: str = ""
     num_v: int = g.vertices
     if num_v == 0:
@@ -21,18 +13,37 @@ def bfs(g: Graph, source: int) -> str:
     queue = MyQueue()
     queue.enqueue(source)
     # Create list to hold visited vertices
+    # and make a node visited whenever you enqueue it into the queue
     visited = set()
     while not queue.is_empty():
         # Dequeue a vertex from the queue
         vertex = queue.dequeue()
         result += str(vertex)
-        print(str(vertex) + " ", end="")
+        # print(str(vertex) + " ", end="")
 
         # If not visited, mark as visited and enqueue it
         temp = g.array[vertex].get_head()
         while temp:
-            visited.add(temp)
-            queue.enqueue(temp)
+            if temp.data not in visited:
+                visited.add(temp.data)
+                queue.enqueue(temp.data)
             temp = temp.next_element
+
+    return result
+
+
+def bfs_simple_dict(graph, start_node):
+    visited = [start_node]
+    queue = [start_node]
+    result = ""
+
+    while queue:
+        s = queue.pop(0)
+        result += s
+
+        for neighbor in graph[s]:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
 
     return result
--- tests/test_ch5graphs/test_graph.py
+++ tests/test_ch5graphs/test_graph.py
@@ -1,11 +1,24 @@
 from ds_coding_interviews_in_python.ch5graphs.graph import Graph
-from ds_coding_interviews_in_python.ch5graphs.main import bfs
+from ds_coding_interviews_in_python.ch5graphs.main import bfs_simple, bfs_simple_dict
 
 
-def test_bfs() -> None:
+def test_bfs_simple() -> None:
     g = Graph(4)
     g.add_edge(0, 1)
     g.add_edge(0, 2)
     g.add_edge(1, 3)
     g.add_edge(2, 3)
-    assert bfs(g, 0) == "0213"
+    assert bfs_simple(g, 0) == "0123"
+
+
+def test_bfs_simple_dict() -> None:
+    # fmt: off
+    graph = {
+        "A": ["B", "C"],
+        "B": ["D", "E"],
+        "C": ["F"],
+        "D": [],
+        "E": ["F"],
+        "F": []
+    }
+    assert bfs_simple_dict(graph, "A") == "ABCDEF"
