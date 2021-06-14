3ee41360863fb15bb812eedbd48e5a2e93dbfa0a
David Dansby <ddansby89@gmail.com>
2021-06-13 17:15:17 -0700

    Add another test on another tree for BST height

--- tests/test_ch6trees/test_chlg4_find_height.py
+++ tests/test_ch6trees/test_chlg4_find_height.py
@@ -22,6 +22,20 @@ def test_find_height(bst) -> None:
     assert find_height(bst.root) == 6
 
 
+def test_find_height_other_tree() -> None:
+    bst = BinarySearchTree(6)
+    bst.iter_insert(4)
+    bst.iter_insert(9)
+    bst.iter_insert(2)
+    bst.iter_insert(5)
+    bst.iter_insert(8)
+    bst.iter_insert(12)
+    bst.iter_insert(10)
+    bst.iter_insert(14)
+
+    assert find_height(bst.root) == 3
+
+
 def test_find_height_null_root() -> None:
     assert find_height(None) == -1
 
